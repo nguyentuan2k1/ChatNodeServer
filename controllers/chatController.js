9795a964aee3515b41dca1881959259a418c1849
nguyentuan <boigaming1411@gmail.com>
2024-09-04 22:10:36 +0700

    update get message by room id

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -166,7 +166,7 @@ exports.takeRoomChat = async (req, res) => {
         const pageSizeMessage = parseInt(page_size_message) || 20;
 
         let messageOfRoom = await ChatMessages.find(
-            { roomID: room._id },
+            { chatID: room._id },
             { userID: 1, message: 1, stampTimeMessage: 1, _id: 0, messageStatus: 1 } // Only select these fields
         )
         .sort({ stampTimeMessage: -1 }) // Sort by stampTimeMessage instead of createdAt
